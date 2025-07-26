46c859c90030bce030c25b758330a434e36f97bf
Yuri M. Avila Souza <121899644+yuriavila18@users.noreply.github.com>
2025-07-26 01:01:57 -0300

    Create ClienteNaoEncontradoException.java

---
+++ excecoes/ClienteNaoEncontradoException.java
@@ -0,0 +1,9 @@
+package Julho.Trabalho2507.excecoes;
+
+public class ClienteNaoEncontradoException extends Exception  {
+    public ClienteNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
+
+
