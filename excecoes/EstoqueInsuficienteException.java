905c82889627cb1237352c63c2452eed97da1ad9
Yuri M. Avila Souza <121899644+yuriavila18@users.noreply.github.com>
2025-07-26 01:02:27 -0300

    Add files via upload

---
+++ excecoes/EstoqueInsuficienteException.java
@@ -0,0 +1,8 @@
+package Julho.Trabalho2507.excecoes;
+
+public class EstoqueInsuficienteException extends Exception  {
+    public EstoqueInsuficienteException(String mensagem) {
+        super(mensagem);
+    }
+}
+
