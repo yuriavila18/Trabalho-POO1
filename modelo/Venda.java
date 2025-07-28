eb0b3e7032c0fe862e5c0d04e4dccfe00a30a52a
Yuri M. Avila Souza <121899644+yuriavila18@users.noreply.github.com>
2025-07-27 23:10:01 -0300

    Update Venda.java

--- modelo/Venda.java
+++ modelo/Venda.java
@@ -17,37 +17,30 @@ public class Venda {
         this.data = data;
         this.valorTotal = calcularTotal();
     }
-
     private double calcularTotal() {
         double total = 0;
         for (ItemCarrinho item : itens) {
             total += item.calcularSubtotal();
-        }
-        return total;
+        } return total;
     }
-
     public String getId() {
         return id;
     }
-
     public Cliente getCliente() {
         return cliente;
     }
-
     public List<ItemCarrinho> getItens() {
         return itens;
     }
-
     public Date getData() {
         return data;
     }
-
     public double getValorTotal() {
         return valorTotal;
     }
-
     @Override
     public String toString() {
-        return "Venda [id=" + id + ", cliente=" + cliente.getNome() + ", data=" + data + ", valorTotal=R$" + String.format("%.2f", valorTotal) + "]";
+        return "Venda [id=" + id + ", cliente=" + cliente.getNome() + ", " +
+                "data=" + data + ", valorTotal=R$" + String.format("%.2f", valorTotal) + "]";
     }
 }
